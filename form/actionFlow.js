288bcb0ae34f0bad319d9e4677e805dfeed857a2
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-02-12 15:59:18 +0530

    u

--- form/actionFlow.js
+++ form/actionFlow.js
@@ -22,36 +22,36 @@ flow = {
 }
 
 
-Example of an action flow
-{
-    actionSteps: [
-        {
-            actionStepIndex: 11				
-		actionStepName: Something1				
-		description: About action step: 11				
-		method: MethodName				
-		arguments: { data1: value1, data2: value2 }
-        },
-        {
-        actionStepIndex: 12				
-		actionStepName: Something2				
-		description: About action step: 12				
-		method: MethodName				
-		arguments: { data1: value1, data2: value2 }				
-		fromPrevious: { data3: 11 }				
-		condition: {
-                completedActionSteps: [11]				
-		compare: [
-                    {
-                        value: 11	
-					equal: true
-                    }	
-					{
-                        value: 11	
-					type: boolean	
+// Example of an action flow
+// {
+//     actionSteps: [
+//         {
+//             actionStepIndex: 11				
+// 		actionStepName: Something1				
+// 		description: About action step: 11				
+// 		method: MethodName				
+// 		arguments: { data1: value1, data2: value2 }
+//         },
+//         {
+//         actionStepIndex: 12				
+// 		actionStepName: Something2				
+// 		description: About action step: 12				
+// 		method: MethodName				
+// 		arguments: { data1: value1, data2: value2 }				
+// 		fromPrevious: { data3: 11 }				
+// 		condition: {
+//                 completedActionSteps: [11]				
+// 		compare: [
+//                     {
+//                         value: 11	
+// 					equal: true
+//                     }	
+// 					{
+//                         value: 11	
+// 					type: boolean	
 						
-			]
-            }				
-	]
-}
+// 			]
+//             }				
+// 	]
+// }
 
