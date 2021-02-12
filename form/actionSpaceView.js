dabbb60dfa4743df89681a7dcc720ad9921e0d68
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-02-12 16:31:54 +0530

    u

--- form/actionSpaceView.js
+++ form/actionSpaceView.js
@@ -12,3 +12,18 @@ var preloadCSSInhead = {
         href: "beauty/action.css"
     }
 }
+
+var actionSpace = {
+    divBlock: [
+        {
+            name: 'div',
+            contentEditable: true,
+            class: 'div-block ',
+            lineNumbers: true,
+            innerText: "Write whatever you can think of...",
+            //mimeMode: ['html', 'richText', 'json', 'css', 'javascript'],
+            //output: ['self', 'output'],
+            state: 'idle',
+        },
+    ]
+}
