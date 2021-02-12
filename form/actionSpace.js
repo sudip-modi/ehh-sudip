088ca7d53c1b30c6a4751bb0574cf75c329f2913
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-02-12 13:41:24 +0530

    u

--- form/actionSpace.js
+++ form/actionSpace.js
@@ -1,3 +1,5 @@
 /**
  * This file contains the json definition of actionSpaceEditor.
+ * This will be used as a prototype to validate any creation of an instance of a new actionSpace.
+ * All the attributes and parameter are defined here.
  */
