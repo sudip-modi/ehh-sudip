1e5ffe9df2d962de018e27a12852948cdda7e011
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-02-12 15:36:58 +0530

    u

--- form/actionSpaceView.js
+++ form/actionSpaceView.js
@@ -1,49 +1,356 @@
 var actionEditor = {
-    style: 'min-height : 200px; width: 400px; border-top: 0px; padding: 21px; overflow: auto;',
-    actionEditorBlock: {
-        name: 'div',
-        contentEditable: true,
-        class: 'actionEditor-block',
-        id: 'actionEditor-block',
-        lineNumbers: true,
-        innerText: "Write whatever you can think of...",
-        //mimeMode: ['html', 'richText', 'json', 'css', 'javascript'],
-        //output: ['self', 'output'],
-        state: 'idle',
-
+    div: [{
+        name: 'button',
+        type: 'button',
+        innerText: 'Sign Up',
+        id: 'sign'
+    }, {
+        name: 'button',
+        type: 'button',
+        innerText: 'Login ',
+        id: 'log'
+    }],
+    style: '/*min-height : 200px;*/ width: max-content; border-top: 0px; padding: 21px; overflow: auto; display:grid',
+    sideNav: {
+        class: 'sidenav',
+        id: 'sidenav',
+        div: []
     },
+    actionView: [
+
+        {
+            name: 'button',
+            type: 'button',
+            class: 'action-view-button',
+            innerHTML: '<i class="fas fa-bars"></i> File'
+        }, {
+            name: 'div',
+            type: 'div',
+            id: 'dropdown-menu',
+            class: 'dropdown-menu',
+            div: [
+                {
+                    name: 'a',
+                    href: "javascript:;",
+                    type: 'button',
+                    id: 'newfile',
+                    innerText: 'New File'
+                }, {
+                    name: 'a',
+                    href: "javascript:;",
+                    type: 'a',
+                    id: 'openfile',
+                    innerText: 'Open File'
+
+                }, {
+
+                    name: 'a',
+                    href: "javascript:;",
+                    type: 'a',
+                    id: 'savefile',
+                    innerText: 'Save File'
+                }, {
+
+                    name: 'a',
+                    href: "javascript:;",
+                    type: 'a',
+                    id: 'export',
+                    innerText: 'Export'
+                },
+                {
+
+                    name: 'a',
+                    href: "javascript:;",
+                    type: 'a',
+                    id: 'import',
+                    innerText: 'Import'
+                }
+            ]
+        }
+
+    ],
     toolBar: [
         {
             name: 'button',
             type: 'div',
-            'innerText': 'add',
-            'id': 'add',
-            'onclick': 'console.log("addButtonClicked")'
+            class: 'topbar-button',
+            'innerHTML': '<i class="fas fa-align-right"></i>',
+            'data-cmd': 'justifyRight'
         },
         {
             name: 'button',
             type: 'div',
-            'innerText': 'BTN',
-            'onclick': 'console.log("i was clicked")'
+            class: 'topbar-button',
+            'innerHTML': '<i class="fas fa-align-left"></i>',
+            'data-cmd': 'justifyLeft'
         },
         {
             name: 'button',
             type: 'button',
-            'innerText': 'BTN',
-            'onclick': 'console.log("i was clicked")'
+            class: 'topbar-button',
+            'innerHTML': '<i class="fas fa-align-center"></i>',
+            'data-cmd': 'justifyCenter'
         },
         {
             name: 'button',
             type: 'button',
-            'innerText': 'BTN',
-            'onclick': 'console.log("i was clicked")'
+            class: 'topbar-button',
+            on: 'click',
+            'innerHTML': '<i class="fas fa-align-justify"></i>',
+            'data-cmd': 'justifyFull'
         },
         {
             name: 'button',
             type: 'button',
-            'innerText': 'BTN',
-            'onclick': 'console.log("i was clicked")'
+            class: 'topbar-button',
+            'innerHTML': '<i class="fas fa-bold"></i>',
+            'data-cmd': 'Bold'
         },
+        {
+            name: 'button',
+            type: "button",
+            class: 'topbar-button',
+            'innerHTML': '<i class="fas fa-italic"></i>',
+            "data-cmd": 'italic'
 
-    ]
+        }, {
+            name: 'button',
+            type: "button",
+            class: 'topbar-button',
+            'innerHTML': '<i class="fas fa-underline"></i>',
+            "data-cmd": 'underline'
+
+        },
+        {
+            name: 'button',
+            type: "button",
+            class: 'topbar-button',
+            'innerHTML': '<i class="fas fa-list-ul"></i>',
+            "data-cmd": 'insertUnorderedList'
+
+        }, {
+            name: 'button',
+            type: "button",
+            class: 'topbar-button',
+            'innerHTML': '<i class="fas fa-list-ol"></i>',
+            "data-cmd": 'insertOrderedList'
+        }, {
+            name: 'button',
+            type: "button",
+            class: 'topbar-button',
+            'innerHTML': '<i class="fas fa-link"></i>',
+            "data-cmd": 'createLink'
+        }, {
+
+            name: 'button',
+            type: "button",
+            class: 'topbar-button',
+            'innerHTML': '<i class="fas fa-code"></i>',
+            "data-cmd": 'showCode'
+        }, {
+            name: 'button',
+            type: "button",
+            class: 'topbar-button',
+            'innerHTML': '<i class="fas fa-redo"></i>',
+            "data-cmd": 'redo'
+        }, {
+            name: 'button',
+            type: "button",
+            class: 'topbar-button',
+            'innerHTML': '<i class="fas fa-undo"></i>',
+            "data-cmd": 'undo'
+        }
+    ],
+    actionEditorBlock:
+        [
+            {
+                name: 'div',
+                class: 'actionEditor-block',
+                id: 'actionEditor-block',
+                lineNumbers: true,
+                //mimeMode: ['html', 'richText', 'json', 'css', 'javascript'],
+                //output: ['self', 'output'],
+                state: 'idle',
+                divBlock: [
+                    {
+                        name: 'div',
+                        contentEditable: true,
+                        class: 'div-block ',
+                        lineNumbers: true,
+                        innerText: "Write whatever you can think of...",
+                        //mimeMode: ['html', 'richText', 'json', 'css', 'javascript'],
+                        //output: ['self', 'output'],
+                        state: 'idle',
+                    },
+                ]
+
+            }
+
+        ],
+    bottomBar:
+        [
+            {
+                name: 'button',
+                type: 'div',
+                class: 'topbar-button',
+                'innerHTML': '<i class="fas fa-heading  "></i>',
+                'data-cmd': 'addHeading'
+            },
+            {
+                name: 'button',
+                type: 'div',
+                class: 'topbar-button',
+                'innerHTML': '<i class="fas fa-paragraph"></i>',
+                'data-cmd': 'para'
+            }, {
+                name: 'button',
+                type: 'div',
+                class: 'topbar-button',
+                'innerHTML': '<i class="fas fa-save"></i>',
+                'data-cmd': 'save'
+            }, {
+                name: 'button',
+                type: 'div',
+                class: 'topbar-button',
+                'innerHTML': '<i class="fas fa-sync"></i>',
+                'data-cmd': 'reload'
+            }, {
+                name: "input",
+                accept: "application/json",
+                type: 'file',
+                "data-cmd": 'loadJson'
+            }
+        ]
+}
+/**
+ * actionSpaceObject is the root Object of actionSpace.
+ * It has all the scripts/divs/json/css needed for the actionSpace to function.
+ * 
+ */
+var actionSpace = {
+    link: {
+        rel: 'stylesheet',
+        id: 'actionEditor.css',
+        type: "text/css",
+        href: "beauty/action.css"
+    },
+    style: '/*min-height : 200px;*/ width: max-content; border-top: 0px; padding: 21px; overflow: auto; display:grid',
+    sideNav: {
+        class: 'sidenav',
+        id: 'sidenav',
+        div: []
+    },
+    actionView: [
+
+        {
+            name: 'button',
+            type: 'button',
+            class: 'action-view-button',
+            innerHTML: '<i class="fas fa-bars"></i> File'
+        }, {
+            name: 'div',
+            type: 'div',
+            id: 'dropdown-menu',
+            class: 'dropdown-menu',
+            div: [
+                {
+                    name: 'a',
+                    href: "javascript:;",
+                    type: 'button',
+                    id: 'newfile',
+                    innerText: 'New File'
+                }, {
+                    name: 'a',
+                    href: "javascript:;",
+                    type: 'a',
+                    id: 'openfile',
+                    innerText: 'Open File'
+
+                }, {
+
+                    name: 'a',
+                    href: "javascript:;",
+                    type: 'a',
+                    id: 'savefile',
+                    innerText: 'Save File'
+                }, {
+
+                    name: 'a',
+                    href: "javascript:;",
+                    type: 'a',
+                    id: 'export',
+                    innerText: 'Export'
+                },
+                {
+
+                    name: 'a',
+                    href: "javascript:;",
+                    type: 'a',
+                    id: 'import',
+                    innerText: 'Import'
+                }
+            ]
+        }
+
+    ],
+    actionEditorBlock:
+        [
+            {
+                name: 'div',
+                class: 'actionEditor-block',
+                id: 'actionEditor-block',
+                lineNumbers: true,
+                //mimeMode: ['html', 'richText', 'json', 'css', 'javascript'],
+                //output: ['self', 'output'],
+                state: 'idle',
+                divBlock: [
+                    {
+                        name: 'div',
+                        contentEditable: true,
+                        class: 'div-block ',
+                        lineNumbers: true,
+                        innerText: "Write whatever you can think of...",
+                        //mimeMode: ['html', 'richText', 'json', 'css', 'javascript'],
+                        //output: ['self', 'output'],
+                        state: 'idle',
+                    },
+                ]
+
+            }
+
+        ],
+    bottomBar:
+        [
+            {
+                name: 'button',
+                type: 'div',
+                class: 'topbar-button',
+                'innerHTML': '<i class="fas fa-heading  "></i>',
+                'data-cmd': 'addHeading'
+            },
+            {
+                name: 'button',
+                type: 'div',
+                class: 'topbar-button',
+                'innerHTML': '<i class="fas fa-paragraph"></i>',
+                'data-cmd': 'para'
+            }, {
+                name: 'button',
+                type: 'div',
+                class: 'topbar-button',
+                'innerHTML': '<i class="fas fa-save"></i>',
+                'data-cmd': 'save'
+            }, {
+                name: 'button',
+                type: 'div',
+                class: 'topbar-button',
+                'innerHTML': '<i class="fas fa-sync"></i>',
+                'data-cmd': 'reload'
+            }, {
+                name: "input",
+                accept: "application/json",
+                type: 'file',
+                "data-cmd": 'loadJson'
+            }
+        ]
 }
--- function/actionSpaceFunction.js
+++ function/actionSpaceFunction.js
@@ -9,4 +9,15 @@
     init Dom..Assingn a ID to each element of Dom.More like Content/Index scripts file
  */
 
+
+
+ehhAppOutput = document.createElement('ehhOutput');
+var temp = new Entity(actionSpace, ehhAppOutput);
+//newBlock = Entity.create(actionEditor.actionEditorBlock,temp.entity)
+//console.log(newBlock)
+//console.log(temp.entity)
+document.getElementsByTagName('body')[0].appendChild(temp.entity);
+console.log("all set and done")
+//console.log(temp.output);
+
  
