d77d946e3e30fdef883e0fe493d3d00dd3f67d96
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-02-13 23:51:04 +0530

    u

--- function/actionEventControllerV2.js
+++ function/actionEventControllerV2.js
@@ -6,16 +6,6 @@ class actionEventController {
     }
 
 
-    on(evt, listener) {
-        (this._events[evt] || (this._events[evt] = [])).push(listener);
-        return this;
-    }
-
-    emit(evt, arg) {
-        (this._events[evt] || []).slice().forEach(lsn => lsn(arg));
-    }
-
-
 
 
 }
