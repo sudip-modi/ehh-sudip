ed0c28be4cc60529788070cefdeb3d676f29a172
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-02-13 18:43:48 +0530

    u

--- function/actionEventController.js
+++ function/actionEventController.js
@@ -1,30 +1,6 @@
 //https://developer.mozilla.org/en-US/docs/Web/API/EventTarget
 //this file handles all the events that are Initialised during Init.
 
-//To make a getReq to a appScript server the queryParameter have to be added to the url using buildEncodedURI.
-//when making a post request a normal post works.
-// function processButtonClick(e) {
-//     e.preventDefault();
-//     console.log(e.target.id);
-//     if (e.target.id === 'get') {
-//         request.method = "GET";
-//         var encodedParam = clientNodeFetch.buildEncodedUri(request);
-//         var url2 = url + "?" + encodedParam;
-//         clientNodeFetch.fetchUrl(url2);
-//     }
-//     if (e.target.id === 'post') {
-//         //request = getRequest;
-//         request.method = "POST";
-//         clientNodeFetch.fetchHttpRequest(url, request);
-//     }
-//
-//
-// }
-//
-//
-// document.getElementById("get").addEventListener("click", processButtonClick);
-// document.getElementById("post").addEventListener("click", processButtonClick);
-//
 
 class actionEventController {
     constructor(context) {
