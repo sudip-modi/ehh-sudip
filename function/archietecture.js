106e527faf05816f33e191b3ba3094182a966833
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-02-14 13:38:56 +0530

    u

---
+++ function/archietecture.js
@@ -0,0 +1,34 @@
+class actionSpace { 
+
+}
+
+class actionEventController extends actionSpace { 
+
+
+}
+
+class actionDataController extends actionSpace {
+
+}
+
+class actionEntityModel extends actionSpace {
+
+}
+
+class actionSpaceView extends actionSpace { 
+
+}
+
+class dataHelpers extends actionSpace { 
+
+}
+
+class process extends actionSpace { 
+
+}
+class operate extends actionSpace { 
+
+}
+class actionEngine extends actionSpace { 
+    
+}
