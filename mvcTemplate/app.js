e76a594d17e566a8e0ba345fe4f29adb6dae4c20
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-02-15 17:17:37 +0530

    u

--- mvcTemplate/app.js
+++ mvcTemplate/app.js
@@ -1,18 +1,24 @@
-
+/**
+ * 
+ */
 var list = document.getElementById('list');
 
 window.addEventListener('load', () => {
-    const model = new ListModel(['node.js', 'react']),
+
+
+
+ //   const model = new ListModel(['node.js', 'react']),
         actionSpace = {
             'list': document.getElementById('list'),
             'addButton': document.getElementById('plusBtn'),
             'delButton': document.getElementById('minusBtn')
         }
+    actionEvent = new ActionEventEmitter(actionSpace)// Controller to be added
+    
 
 
+       //  view = new ListView(model, actionSpace),
+      //  controller = new ListController(model, view);
 
-    view = new ListView(model, actionSpace),
-        controller = new ListController(model, view);
-
-    view.show();
+   // view.show();
 });
--- mvcTemplate/event.js
+++ mvcTemplate/event.js
@@ -1,6 +1,6 @@
 
 
-class EventEmitter {
+class ActionEventEmitter {
     constructor() {
         this._events = {};
     }
