dc67e29e4f17d8dc38fde4c667144eac7dfe67e7
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-02-18 14:33:25 +0530

    u

--- ver4/function/actionController.js
+++ ver4/function/actionController.js
@@ -39,27 +39,20 @@ class ActionController extends ActionEvent {
     onKeyPress(entity) {
         var currentSelection = window.getSelection();
         var currentCaret = currentSelection.anchorOffset;
-        if (!currentInputRange) { var currentInputRange = [] };
-            var cartetAtPosIndex = Caret.getCaretIndex(entity.target);
-            var cartetAtPos = Caret.getCaretPos(entity);
-            var match = isIn(entity.key, snippets);
-            if (match.length === 1) { // is a exact Match. To be used for Space, {,[,(
-              
-               entity.preventDefault();
-                console.log("match",match[0].prefix ,match[0].body);
-                var response = Entity.insert(currentSelection.baseNode.textContent, currentSelection.anchorOffset, entity.key + match[0].body); 
-                currentSelection.baseNode.textContent = response;
-                Caret.moveCaret(window, currentCaret + 1);
-        }
+
+        entity.preventDefault(entity);
+
+        var response = Entity.insert(currentSelection.baseNode.textContent, currentSelection.anchorOffset, entity.key);
+      //  console.log(response)
+        currentSelection.baseNode.textContent = response;
+        Caret.moveCaret(window, currentCaret + 1);
 
         if (entity.keyCode == 32) { 
-           // entity.preventDefault();
-       
-            //console.log(currentSelection.focusNode, currentSelection.focusNode.textContent,entity.target.innerText)
+      //     console.log("space key pressed")
             var focusText = currentSelection.focusNode.textContent;
           //  var focusText = entity.target.innerText;
             
-        //    console.log("focusText",focusText)
+            console.log("focusText",focusText)
            // Caret.moveCaret(window, lastSpace);
 
             var index = focusText.indexOf(currentCaret);
@@ -74,7 +67,7 @@ class ActionController extends ActionEvent {
             else {
                 console.log(preText) ;
             }
-            //console.log("space key pressed", lastWord);
+            console.log("space key pressed", lastWord);
             var match = isIn(lastWord, snippets);
     
             if (match.length === 1) { // is a exact Match. To be used for Space, {,[,(
