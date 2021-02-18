996687bcbc09c2c0effa842fd6244212199d88db
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-02-19 01:11:27 +0530

    u

--- ver4/function/actionSpace.js
+++ ver4/function/actionSpace.js
@@ -14,10 +14,11 @@ class ActionSpace {
     }
 }
 
-document.onload = onload;
+document.onload = onload();
+
 function onload() { 
     var newActionSpace = new ActionSpace(basicLayout);
 }
 
-
+//var newActionSpace = new ActionSpace(basicLayout);
 //var newLayout = new Entity(actionSpaceLayoutV1, document.getElementsByTagName('body')[0])
