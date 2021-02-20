76d0664c88577401516fb681ef4a7916e5d163b7
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-02-20 11:32:56 +0530

    u

--- ver5/function/actionSpaceMerged_V5.js
+++ ver5/function/actionSpaceMerged_V5.js
@@ -4,7 +4,58 @@ var actionSpaceSchema = {
 }
 
 //console.log("app", app)
+class ActionEvent {
+    constructor(elements4Event, entity) {
+        this._events = {};
+        this._elements = elements4Event;
+        window.addEventListener('message', e => this.emit('message', e));
+        this.on('message', e => this.handleEvent(e));
 
+      //  this.createListeners(entity);
+        //  console.log(elements4Event)
+        this.on('selection', e => this.onSelection(e));
+        this.on('change', e => this.onSelection(e));
+        this.on('keypress', e => this.onKeyPress(e));
+        this.on('keyup', e => this.onKeyUp(e));
+        this.on('click', e => this.onClick(e));
+        this.on('insertText', e => this.insertText(e));
+        this.on('delButtonClicked', e => this.del(e));
+
+    }
+    createListeners(entity) {
+        console.log(entity)
+        let events = dataHelpers.find(entity, 'on')
+        console.log(events)
+        events.forEach((evt) => {
+            entity[evt] = this.handleEvent
+        })
+    }
+
+    addListener(eventName, fn) {
+        this._events[eventName] = this._events[eventName] || [];
+        this._events[eventName].push(fn);
+        return this;
+    }
+    on(eventName, fn) {
+        return this.addListener(eventName, fn);
+    }
+    emit(eventName, ...args) {
+        let fns = this._events[eventName];
+        //  console.log("Emitted",eventName)
+        if (!fns) return false;
+        fns.forEach((f) => {
+            f(...args);
+        });
+        return true;
+    }
+    handleEvent(event) {
+        // console.log("handler", event.type, event.target.getAttribute('name'))
+        //  window.postMessage()
+
+        //filter the registerd events paired with Target
+
+    }
+}
 class Entity { 
     constructor(input, output) {
         console.log("entity",input,output)
@@ -126,51 +177,35 @@ class ActionView {
     }
 
 }
-class ActionController {
-    constructor(model, view,actionEvent) {
+class ActionController extends ActionEvent {
+    constructor(model, view, actionEvent) {
+        super()
         this.model = model
         this.view = view
         this.actionEvent = actionEvent
+        
+
+        window.addEventListener('change', e => this.emit('change', e));
+        window.addEventListener('event', e => this.emit('click', e))
+        window.addEventListener('click', e => this.emit('click', e));
+        window.addEventListener('keypress', e => this.emit('keypress', e));
+        window.addEventListener('keyup', e => this.emit('keyup', e));
+    }
+    conductEvent(event) { 
+        console.log("handler", event.type, event.target)
+        
+    }
+    onClick(event) { 
+
     }
 }
+ var actionSpaceElement = document.getElementById('actionSpace#1');
+ console.log(actionSpaceElement);
+const actionEntity = new Entity(basicLayout, {});
+const actionViewInstance = new ActionView(basicLayout, actionSpaceElement);
+const actionEventInstance = new ActionEvent(actionSpaceElement, this);
+const actionSpaceInstanceApp = new ActionController(actionEntity, actionViewInstance, actionEventInstance);
 
 
-class ActionEvent {
-    constructor(elements4Event) {
-        this._events = {};
-        this._elements = elements4Event;
-        //  console.log(elements4Event)
-        this.on('selection', e => this.onSelection(e));
-        this.on('change', e => this.onSelection(e));
-
-        this.on('keypress', e => this.onKeyPress(e));
-        this.on('keyup', e => this.onKeyUp(e));
-        this.on('click', e => this.onClick(e));
-        this.on('insertText', e => this.insertText(e));
-        this.on('delButtonClicked', e => this.del(e));
-
-    }
-
-    addListener(eventName, fn) {
-        this._events[eventName] = this._events[eventName] || [];
-        this._events[eventName].push(fn);
-        return this;
-    }
-    on(eventName, fn) {
-        return this.addListener(eventName, fn);
-    }
-    emit(eventName, ...args) {
-        let fns = this._events[eventName];
-        //  console.log("Emitted",eventName)
-        if (!fns) return false;
-        fns.forEach((f) => {
-            f(...args);
-        });
-        return true;
-    }
-}
-var actionSpaceElement = document.getElementById('actionSpace#1');
-console.log(actionSpaceElement);
-const actionSpaceApp = new ActionController(new Entity(basicLayout, {}), new ActionView(basicLayout, actionSpaceElement), new ActionEvent(actionSpaceElement))
-console.log("actionSpaceApp", actionSpaceApp)
-
+// const actionSpaceApp = new ActionController(), new ActionView(basicLayout, actionSpaceElement), new ActionEvent(actionSpaceElement,window))
+// console.log("actionSpaceApp", actionSpaceApp)
