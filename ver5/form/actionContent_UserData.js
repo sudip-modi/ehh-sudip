ddcd949dd9aad517f84dd2762074b69a8edee5b4
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-02-20 15:33:14 +0530

    u

--- ver5/form/actionContent_UserData.js
+++ ver5/form/actionContent_UserData.js
@@ -4,7 +4,7 @@ var actionContent = {
     id:'sampleNote#1',
     innerHTML: "https://github.com/bronzwikgk/everything-Happens-Here-Ver-O.1 https://web.dev/local-fonts/ I was changed <br> https://www.youtube.com/watch?v=Troaz3rGzTY <br>https://bgrins.github.io/devtools-snippets / <br>https://htmldom.dev/make-a-resizable-element/<br> https://htmldom.dev/create-resizable-split-views/",
     class: 'richText',
-    'prefixInView': 'name'
+    'before': 'drag_indicator'
     //  contentEditable: true
 }
 
