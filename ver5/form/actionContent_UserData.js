1a9e55ac043be83d340e1fd81507ef135b3efc39
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-02-21 03:56:19 +0530

    u

--- ver5/form/actionContent_UserData.js
+++ ver5/form/actionContent_UserData.js
@@ -2,7 +2,7 @@
 var actionStorySample = {
     name: 'actionStorySample',
     id:'actionStorySample',
-    innerHTML: "https://github.com/bronzwikgk/everything-Happens-Here-Ver-O.1 https://web.dev/local-fonts/ I was changed <br> https://www.youtube.com/watch?v=Troaz3rGzTY <br>https://bgrins.github.io/devtools-snippets / <br>https://htmldom.dev/make-a-resizable-element/<br> https://htmldom.dev/create-resizable-split-views/",
+    innerHTML: "https://alistapart.com/article/javascript-mvc/ https://github.com/bronzwikgk/everything-Happens-Here-Ver-O.1 https://web.dev/local-fonts/ I was changed <br> https://www.youtube.com/watch?v=Troaz3rGzTY <br>https://bgrins.github.io/devtools-snippets / <br>https://htmldom.dev/make-a-resizable-element/<br> https://htmldom.dev/create-resizable-split-views/",
     class: 'editable actionContent',
     'before': 'name'
     //  contentEditable: true
