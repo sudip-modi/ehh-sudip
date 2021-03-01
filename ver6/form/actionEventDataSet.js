e4fff565b7f0a6b63e2bb798202c92a62adaa9db
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-03-01 19:29:59 +0530

    u

---
+++ ver6/form/actionEventDataSet.js
@@ -0,0 +1,4 @@
+let event = {
+    'origin': ["mouse", "window", "ehh", "HTTP", "keyBoard"],
+    'typeOfEvents': ["AnimationEvent", "ClipboardEvent", "DragEvent", "FocusEvent", "HashChangeEvent", "InputEvent", " KeyboardEvent", "MouseEvent", "PageTransitionEvent", " PopStateEvent", "ProgressEvent", "StorageEvent", "TouchEvent", "TransitionEvent", "UiEvent", "WheelEvent"]
+}
