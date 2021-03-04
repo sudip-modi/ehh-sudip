7d75bb6a0b1a750510d76af81ae35d602875eb97
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-03-04 16:25:57 +0530

    u

--- ver6/function/actionDataBaseV2.js
+++ ver6/function/actionDataBaseV2.js
@@ -1,18 +1,18 @@
 //indexedDB === database
 //contains object === tables 
 
-
+var dbVersion = 1;
 class actionDataSets { 
     static openDataBase(databaseName, dbVersion) {
-        var request = window.indexedDB.open(databaseName, dbVersion);
-        request.onerror = function (event) {
+        var databaseWip = window.indexedDB.open(databaseName, dbVersion);
+        databaseWip.onerror = function (event) {
             // Do something with request.errorCode!
             console.log("Why didn't you allow my web app to use IndexedDB?!");
         };
-        request.onsuccess = function (event) {
+        databaseWip.onsuccess = function (event) {
             // Do something with request.result!
-            var db = request.result;
-            console.log("success",db);
+            var db = databaseWip.result;
+            console.log("open DataBase success",db);
             db.onerror = function (event) {
                 // Generic error handler for all errors targeted at this database's
                 // requests!
@@ -20,11 +20,11 @@ class actionDataSets {
             };
             return db;
         };
-        request.onupgradeneeded = function (event) {
+        databaseWip.onupgradeneeded = function (event) {
             // the existing database version is less than 2 (or it doesn't exist)
-            let db = request.result;
+            let db = databaseWip.result;
             console.log("onupgrade", db);
-            var objectStore = db.createObjectStore("entity");
+         //   var objectStore = db.createObjectStore("entity");
             return db;
             switch (event.oldVersion) { // existing db version
                 case 0:
@@ -36,23 +36,22 @@ class actionDataSets {
             }
         };
 
-    }
-   
+    }  
     static deleteDatabase(databaseName) {
         let deleteRequest = indexedDB.deleteDatabase(databaseName);
-
     }
-    static createStore(databaseName, dbVersion,storeName) { 
-        var request = window.indexedDB.open(databaseName, dbVersion);
-        request.onerror = function (event) {
+    static createStore(databaseName, dbVersion, storeName) { 
+        console.log("creating store")
+        var databaseWip = window.indexedDB.open(databaseName, dbVersion);
+        databaseWip.onerror = function (event) {
             // Do something with request.errorCode!
             console.log(request.error.message)
             console.log("Why didn't you allow my web app to use IndexedDB?!");
         };
-        request.onsuccess = function (event) {
+        databaseWip.onsuccess = function (event) {
             // Do something with request.result!
-            var db = request.result;
-            console.log("success", db);
+            var db = databaseWip.result;
+            console.log(" databaseWip open success", db);
             db.onerror = function (event) {
                 // Generic error handler for all errors targeted at this database's
                 // requests!
@@ -60,22 +59,23 @@ class actionDataSets {
             };
             return db;
         };
-        request.onupgradeneeded = function (event) {
+        databaseWip.onupgradeneeded = function (event) {
             // the existing database version is less than 2 (or it doesn't exist)
-            let db = request.result;
+            let db = databaseWip.result;
             console.log("onupgrade", db);
 
-            var oldVersion = request.oldVersion;
-            var newVersion = request.newVersion || db.version;
+            var oldVersion = databaseWip.oldVersion;
+            var newVersion = databaseWip.newVersion || db.version;
             console.log('db updated fro version ,', oldVersion, "to", newVersion);
             if (db.objectStoreNames.contains(storeName)) {
                 console.log("dublicate found for ", storeName)
-            }else if (!db.objectStoreNames.contains(storeName)) {
+            }
+            if (!db.objectStoreNames.contains(storeName)) {
                 var objectStore = db.createObjectStore(storeName, { autoIncrement: true });
-                db.close();
+               // db.close();
             }
             
-            return objectStore;
+          //  return objectStore;
             
         };
 
@@ -85,7 +85,6 @@ class actionDataSets {
         request.onerror = function (event) {
             // Do something with request.errorCode!
             console.log(request.error.message)
-            console.log("Why didn't you allow my web app to use IndexedDB?!");
         };
         request.onsuccess = function (event) {
             // Do something with request.result!
@@ -96,16 +95,12 @@ class actionDataSets {
                 // requests!
                 console.error("Database error: " + event.target.errorCode);
             };
-            return db;
+           // return db;
         };
         request.onupgradeneeded = function (event) {
             // the existing database version is less than 2 (or it doesn't exist)
             let db = request.result;
-            //console.log("onupgrade", db);
-
-         //   var oldVersion = request.oldVersion;
-           // var newVersion = request.newVersion || db.version;
-            //console.log('db updated fro version ,', oldVersion, "to", newVersion);
+        
             if (db.objectStoreNames.contains(storeName)) {
                
                 db.deleteObjectStore(storeName);
@@ -116,36 +111,78 @@ class actionDataSets {
             }
 
         };
-
     }
-    static add2Store(storeName,mode) { 
-        let tx = db.transaction(storeName, mode);
-        tx.onerror = function (event) {
+    
+    static makeTransaction(storeName, mode) {
+    let tx = db.transaction(storeName, mode);
+    tx.onerror = (err) => {
+        console.warn(err);
+    };
+        return tx;
+    }
+    static add2Store(databaseName,storeName,entity,entityValue) { 
+        
+        console.log("creating store")
+        var databaseWip = window.indexedDB.open(databaseName, dbVersion);
+        databaseWip.onerror = function (event) {
             // Do something with request.errorCode!
-            console.warn(err)
-            //console.log("Why didn't you allow my web app to use IndexedDB?!");
-        }
-        tx.onsuccess = function (event) {
+            console.log(request.error.message)
+            console.log("Why didn't you allow my web app to use IndexedDB?!");
+        };
+        databaseWip.onsuccess = function (event) {
             // Do something with request.result!
-            console.log("successful transaction",event, );
-        }
-        let store = tx.objectStore(storeName);
-        var txRequest = store.add("value");
-        tx.onsuccess = function (event) { 
-            console.log("successful TransactionReq");
-        }
+            var db = databaseWip.result;
+            console.log(" databaseWip open success", db);
+            let transaction = db.transaction(storeName, "readwrite"); // (1)
+            // get an object store to operate on it
+            //let entityTransaction = transaction.objectStore(entity); // (2)
 
+
+            //let request = entityTransaction.add(entityValue);
+
+
+
+
+
+
+
+            db.onerror = function (event) {
+                // Generic error handler for all errors targeted at this database's
+                // requests!
+                console.error("Database error: " + event.target.errorCode);
+            };
+            return db;
+        };
+        databaseWip.onupgradeneeded = function (event) {
+            // the existing database version is less than 2 (or it doesn't exist)
+            let db = databaseWip.result;
+            console.log("onupgrade", db);
+
+            var oldVersion = databaseWip.oldVersion;
+            var newVersion = databaseWip.newVersion || db.version;
+            console.log('db updated fro version ,', oldVersion, "to", newVersion);
+            if (db.objectStoreNames.contains(storeName)) {
+                console.log("dublicate found for ", storeName)
+            }
+            if (!db.objectStoreNames.contains(storeName)) {
+                var objectStore = db.createObjectStore(storeName, { autoIncrement: true });
+                // db.close();
+            }
+
+            //  return objectStore;
+
+        };
     }
 
 }
 
 
 //var filesDatabase = actionDataSets.openDataBase("filesDatabase", 1);
-//var objectStoreLive = actionDataSets.createStore("filesDatabase", 3, "recentFilesStore");
-
-actionDataSets.deleteStore("filesDatabase", 11, "recentFiles");
-actionDataSets.deleteDatabase("filesDatabase");
-var request = objectStore.clear();
+//var objectStoreLive = actionDataSets.createStore("filesDatabase",3, "recentFilesStore");
+actionDataSets.add2Store("files","recentFileStore","filename","fileContent")
+//actionDataSets.deleteStore("filesDatabase", 11, "recentFiles");
+//actionDataSets.deleteDatabase("filesDatabase");
+//var request = objectStore.clear();
 //console.log("fileDataBase",filesDatabase)
 //actionDataSets.openDataBase("folders");
 //actionDataSets.openDataBase("systemDataSet");
