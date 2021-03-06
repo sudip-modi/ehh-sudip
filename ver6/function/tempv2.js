20a73b9ffd18cca61adf8293d3496864eb6c9426
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-03-06 13:54:56 +0530

    u

--- ver6/function/tempv2.js
+++ ver6/function/tempv2.js
@@ -3,11 +3,43 @@ var actionRequest = {
     'entityObjectModel': 'document',
     'methodName': 'getElementById',
     'entity': 'actionStory1'
-
 }
 
-var classtoCall = actionRequest.entityObjectModel;
-var method2Call = actionRequest.methodName;
-var entity2Call = actionRequest.entity;
-var response = classtoCall[method2Call].call(entity2Call);
-console.log(response);
+var classtoCall = document;
+var method2Call = "getElementById";
+var entity2Call = "actionStory1";
+
+
+const exeReq = function (req) {
+    //this = actionRequest.entityObjectModel;
+  //  window[actionRequest.entityObjectModel];
+    var requestbuild = "return " + req.entityObjectModel + "." + req.methodName + "(" + actionRequest.entity + ")";
+    console.log(requestbuild);
+    var response = new Function( requestbuild);
+    var output = response();
+
+ //   var response = this[actionRequest.methodName](actionRequest.entity);
+    console.log(output);
+}
+exeReq(actionRequest);
+
+var classtoCall = document;
+var method2Call = "getElementById";
+var entity2Call = "actionStory1";
+var codeToExecute = "return document.getElementById('actionStory1')";
+var tmpFunc = new Function(codeToExecute);
+var t = tmpFunc();
+console.log(t)
+
+
+function executeFunctionByName(functionName, context /*, args */) {
+    var args = Array.prototype.slice.call(arguments, 2);
+    var namespaces = functionName.split(".");
+    var func = namespaces.pop();
+    for (var i = 0; i < namespaces.length; i++) {
+        context = context[namespaces[i]];
+    }
+    return context[func].apply(context, args);
+}
+
+console.log(executeFunctionByName(method2Call,classtoCall,entity2Call))
