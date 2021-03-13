0a93cbb3131c364aea721ff3869c3c2f79dc1d7c
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-03-13 15:24:10 +0530

    u

---
+++ ver6/function/actionEntityV9.js
@@ -0,0 +1,14 @@
+var setEntityReq = {
+    objectModel: ActionEntityV9,
+    method: 'setEntity',
+    arguments: ['input', 'output', 'key']
+}
+
+
+var copy2Req = {
+    objectModel: ActionEntityV9,
+    method: 'copy2',
+    arguments: [, entity2,],
+    response: []
+
+}
