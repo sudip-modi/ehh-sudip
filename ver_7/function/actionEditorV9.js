faf73e9b265abe50f2104da4ebc16c1b7654d123
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-03-16 21:00:48 +0530

    u

--- ver_7/function/actionEditorV9.js
+++ ver_7/function/actionEditorV9.js
@@ -3,6 +3,22 @@ class ActionEditorV9 {
         
         
     }
+    getCaretIndex(element) {
+        let position = 0;
+        const isSupported = typeof window.getSelection !== "undefined";
+        if (isSupported) {
+            const selection = window.getSelection();
+            if (selection.rangeCount !== 0) {
+                const range = window.getSelection().getRangeAt(0);
+                const preCaretRange = range.cloneRange();
+                preCaretRange.selectNodeContents(element);
+                preCaretRange.setEnd(range.endContainer, range.endOffset);
+                position = preCaretRange.toString().length;
+            }
+        }
+        console.log("carretPostion",position)
+        return position;
+    }
     textBeforeCaret() {
         if (this.caretIndex == 0) {
             return "";
