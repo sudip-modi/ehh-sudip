80745317aaf1ab935cd65aba0eb801c85440dd39
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-03-23 17:19:28 +0530

    u

--- ver_7/form/appViewTemplatesV3.js
+++ ver_7/form/appViewTemplatesV3.js
@@ -132,6 +132,7 @@ var actionSpaceContainerModelSchemaVer_2 = {
                                     name: 'div',
                                     'class': "collection_name",
                                     'textContent': 'open folder',
+                                    'onclick':"processFS.OpenDirectory(event);"
                                 }
 
                             },
@@ -527,4 +528,55 @@ var actionSpaceContainerModelSchemaVer_2 = {
         }
     }
 
+}
+
+
+var sidebarJSON = {
+    // 'sidebar':{
+    'name': 'div',
+    'class': 'row',
+    'menu': {
+        'name': 'div',
+        'class': 'sidebar col-md-4',
+        'frontEnd': {
+            'name': 'ul',
+            'id': 'frontEnd',
+            'workspace': {
+                'name': 'li',
+                'span': {
+                    'name': 'span',
+                    'class': 'caret',
+                    'innerText': 'Workspace'
+                },
+                'list': {
+                    'name': 'ul',
+                    'class': 'nested',
+                    'id': 'workspace',
+                }
+            }
+        }
+    }
+    //  }
+};
+var directoryJSON = {
+    'li': {
+        'name': 'li',
+        'span': {
+            'name': 'span',
+            'class': 'caret',
+            'innerText': '', //inner Text will be included
+        },
+        'list': {
+            'name': 'ul',
+            'class': 'nested',
+            'id': '' //id - Unique ID with which directory handle of this folder can ke retrieved from indexDB
+        }
+    }
+}
+var fileJSON = {
+    'name': 'li',
+    'id': '', //id - Unique ID with which file handle of this file retrieved from indexDB
+    'class': 'file',//used for opening file
+    'innerText': ''
+    //innerText - name of the file
 }
