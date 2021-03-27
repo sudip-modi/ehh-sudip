038a8020ee420aa1373ea0a23f010e30bb265d84
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-03-27 11:50:07 +0530

    Update appDataSet.js

--- ver_8/form/appDataSet.js
+++ ver_8/form/appDataSet.js
@@ -12,3 +12,21 @@ var HTMLElementList = ["a", "abbr", "address", "area", "article", "aside", "audi
 //   text: string,
 // }
 
+var serverNodeRequest = {
+    "parameters": {},
+    "parameter": {},
+    "contentLength": -1,
+    "contextPath": "",
+    "queryString": ""
+}
+
+var serNodeResponse = {
+    "pathInfo": "exec",
+    "contextPath": "",
+    "parameter": {},
+    "contentLength": -1,
+    "queryString": "",
+    "parameters": {}
+}
+
+
