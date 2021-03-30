18abb3facf82757aa40946eb2a3a25944ea3e032
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-03-30 16:51:39 +0530

    u

---
+++ input/spreadsheetObject.js
@@ -0,0 +1,10 @@
+var spreadsheetResource ={
+ "spreadsheetId": string,
+ "properties": {object (SpreadsheetProperties)},
+ "sheets": [ { object (Sheet)}],
+ "namedRanges": [ { object (NamedRange)}],
+ "spreadsheetUrl": string,
+ "developerMetadata": [ { object (DeveloperMetadata) }],
+ "dataSources": [{ object (DataSource)}],
+ "dataSourceSchedules": [ { object (DataSourceRefreshSchedule)}]
+}
