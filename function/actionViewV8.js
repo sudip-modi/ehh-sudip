d24870403fd77ee58fd0a1633b28aabd06a2e25d
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-03-30 19:22:57 +0530

    u

--- function/actionViewV8.js
+++ function/actionViewV8.js
@@ -26,4 +26,63 @@ class ActionView {
     target.innerHTML = inputElem;
   }
 }
+
+
+/**
+ * @file get/set caret position and insert text
+ * @author islishude
+ * @license MIT
+ */
+ class Caret {
+    /**
+     * get/set caret position
+     * @param {HTMLColletion} target
+     */
+    constructor(target) {
+        this.isContentEditable = target && target.contentEditable
+        this.target = target
+        //console.log("CaretCreated ",target.tagName);
+    }
+
+
+    static moveCaret(win, charCount) {
+        var sel, range;
+        if (win.getSelection) {
+            // IE9+ and other browsers
+            sel = win.getSelection();
+            if (sel.rangeCount > 0) {
+                var textNode = sel.focusNode;
+                var newOffset = sel.focusOffset + charCount;
+                sel.collapse(textNode, Math.min(textNode.length, newOffset));
+            }
+        } else if ((sel = win.document.selection)) {
+            // IE <= 8
+            if (sel.type != "Control") {
+                range = sel.createRange();
+                range.move("character", charCount);
+                range.select();
+            }
+        }
+    }
+
+  static getCaretCoordinates() {
+    let x = 0,
+        y = 0;
+    const isSupported = typeof window.getSelection !== "undefined";
+    if (isSupported) {
+        const selection = window.getSelection();
+        if (selection.rangeCount !== 0) {
+            const range = selection.getRangeAt(0).cloneRange();
+            range.collapse(true);
+            const rect = range.getClientRects()[0];
+            if (rect) {
+                x = rect.left;
+                y = rect.top;
+            }
+        }
+    }
+    return { x, y };
+}
+}
+
     
