85739e7bdd4d4d956989b0766ab3564bc5423de8
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-04-08 01:55:30 +0530

    u

--- function/dashboardV1.js
+++ function/dashboardV1.js
@@ -1,17 +1,18 @@
 var response2;
+indexDB.set('recentStories', recentStories);
 indexDB.set('userDashboard', userDashboard);
-var viewModel = indexDB.get('userDashboard');
-response2 = viewModel.then((viewModelFromIndexDb) => {
-    //  console.log("here", viewModelFromIndexDb);
-    // expected output: "Success!"
-    var response = viewModelFromIndexDb;
-    console.log(response);
+// var viewModel = indexDB.get('userDashboard');
+// response2 = viewModel.then((viewModelFromIndexDb) => {
+//     //  console.log("here", viewModelFromIndexDb);
+//     // expected output: "Success!"
+//     var response = viewModelFromIndexDb;
+//     console.log(response);
 
 
-    //    test(response)
-    return response;
-});
-console.log(response2)
+//     //    test(response)
+//     return response;
+// });
+// console.log(response2)
 var actionSpaceElementInstanceIndom = document.getElementById('actionSpaceContainer');
 var actionSpaceInstance = new Entity(userDashboard, actionSpaceElementInstanceIndom);
 console.log(actionSpaceInstance.entity);
